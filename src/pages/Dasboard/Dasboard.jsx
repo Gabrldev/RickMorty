f44a21ee4b4199d0400475b7492234084663b3e0
Gabriel <newaydsg@gmail.com>
2023-04-06 13:25:06 -0400

    ✅Fix personajes repitidos

--- src/pages/Dasboard/Dasboard.jsx
+++ src/pages/Dasboard/Dasboard.jsx
@@ -6,41 +6,38 @@ import style from "./dasboard.module.css";
 import toast, { Toaster } from "react-hot-toast";
 
 function Dashboard() {
+  const URL_BASE = "https://be-a-rym.up.railway.app/api/character";
+  const API_KEY = "01c65889effb.1b54c5795354ee1b48e5";
   function onSearch(id) {
-    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
-      ({ data }) => {
+    if (id > 826 || id < 1) {
+      const notify = () => toast.error("¡No hay personajes con este ID!");
+      notify();
+    }
+    axios(`${URL_BASE}/${id}?key=${API_KEY}`).then(({ data }) => {
+      if (characters.find((char) => char.id === data.id)) {
+        const notify = () => toast.error("¡Ya existe este personaje!");
+        notify();
+      } else {
         if (data.name) {
-          if (!characters.find((char) => char.id === data.id)) {
-            setCharacters((oldChars) => [...oldChars, data]);
-            const notify = () => toast.success("Personaje agregado con exito!");
-            notify();
-          } else {
-            const notify = () => toast.error("¡Ya existe este personaje!");
-            notify();
-          }
-        } else {
-          alert("¡No hay personajes con este ID!");
+          setCharacters((oldChars) => [...oldChars, data]);
+          const notify = () => toast.success("Personaje agregado con exito!");
+          notify();
         }
       }
-    );
+    });
   }
-  function getRandon(id){
-    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
-      ({ data }) => {
+  function getRandon(id) {
+    axios(`${URL_BASE}/${id}?key=${API_KEY}`).then(({ data }) => {
+      if (characters.find((char) => char.id === data.id)) {
+        getRandon(Math.floor(Math.random() * 826));
+      } else {
         if (data.name) {
-          if (!characters.find((char) => char.id === data.id)) {
-            setCharacters((oldChars) => [...oldChars, data]);
-            const notify = () => toast.success("Personaje agregado con exito!");
-            notify();
-          } else {
-            const notify = () => toast.error("¡Ya existe este personaje!");
-            notify();
-          }
-        } else {
-          alert("¡No hay personajes con este ID!");
+          setCharacters((oldChars) => [...oldChars, data]);
+          const notify = () => toast.success("Personaje agregado con exito!");
+          notify();
         }
       }
-    );
+    });
   }
   const onClose = (id) => {
     setCharacters(characters.filter((char) => char.id !== id));
@@ -52,7 +49,7 @@ function Dashboard() {
 
   return (
     <div className={style.container}>
-      <Nav onSearch={onSearch}  getRandon={getRandon}/>
+      <Nav onSearch={onSearch} getRandon={getRandon} />
       <Toaster />
       <Cards characters={characters} onClose={onClose} />
     </div>
