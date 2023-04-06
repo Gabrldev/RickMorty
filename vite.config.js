73fc5535499c166f7422cc155dd6a021cf7ce496
Gabriel <newaydsg@gmail.com>
2023-04-05 22:37:50 -0400

    🔵Postcss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}) // add options if needed
+      ],
+    }
+  }
 })
