8717c25b46d89c7d6b58f7744afb4295123ca186
Gabriel <newaydsg@gmail.com>
2023-04-15 14:24:28 -0400

    ✅

--- src/client/client.js
+++ src/client/client.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://uzpmvpjlwmktjtipuxxf.supabase.co'
-const supabaseKey = process.env.KEY_SUPABASE
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6cG12cGpsd21rdGp0aXB1eHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA5MTgwNTIsImV4cCI6MTk5NjQ5NDA1Mn0.3It7FOK-lEE3CQ4TzfPxE3cXxDBGOCBUVnzqHggZCuA'
 export const supabase = createClient(supabaseUrl, supabaseKey)
